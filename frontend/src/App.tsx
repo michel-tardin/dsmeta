bbe70c0f7b430f1e6e972b4d3b823c3d6553b825
Michel Tardin <micheltardin@gmail.com>
2022-07-16 03:52:07 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,12 @@
+import NotificationButton from "./Components/NotificationButton"
+
 function App() {
     return (
-    
+    <>
       <h1>Olá Mundo!</h1>
-   
+   <NotificationButton />
+   <NotificationButton />
+   </>
   )
 }
 
